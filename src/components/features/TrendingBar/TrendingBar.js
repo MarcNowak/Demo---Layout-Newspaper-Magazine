8699f18c88ff1901d6f124fe20fb4a838f0de6f1
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-02-03 20:09:37 +0100

    Add Task_005_TrendingBar - in progress

--- src/components/features/TrendingBar/TrendingBar.js
+++ src/components/features/TrendingBar/TrendingBar.js
@@ -1,11 +1,42 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 // import PropTypes from 'prop-types';
 
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
 import styles from './TrendingBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 const TrendingBar = () => (
   <div className={styles.root}>
-    <p>Trending Bar</p>
+    <Grid>
+      <Row beetween='md' middle='xs'>
+        <Col md={4} lg={2}>
+          <div className={styles.trendingNow}>
+            <span>trending now</span>
+          </div>
+        </Col>
+        <Col md={6} lg={8}>
+          <div className={styles.trendingDesc}>
+            <span>The Weirdest Places Ashes Have Been Scattered in South America</span>
+          </div>
+        </Col>
+        <Col md={4} lg={2}>
+          <div className={styles.prevNext}>
+            <a href='#'>
+              <FontAwesomeIcon icon={faAngleDoubleLeft}></FontAwesomeIcon>
+            </a>
+            <a href="#">
+              <FontAwesomeIcon icon={faAngleDoubleRight}></FontAwesomeIcon>
+            </a>
+          </div>
+        </Col>
+      </Row>
+    </Grid>
   </div>
 );
 

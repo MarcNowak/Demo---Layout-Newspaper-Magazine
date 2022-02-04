52d3e82216b23471c21907f4897fbf131379a2ba
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-02-04 22:30:33 +0100

    Add Task TEST - approach modification

--- src/components/common/MiddleArticles/MiddleArticles.js
+++ src/components/common/MiddleArticles/MiddleArticles.js
@@ -2,12 +2,24 @@ import React from 'react';
 
 // import PropTypes from 'prop-types';
 
+import styles from './MiddleArticles.module.scss';
+
 import Fitness from '../Fitness';
+import MiddleRightColumn from '../../features/MiddleRightColumn/MiddleRightColumn';
 
 const MiddleArticles = () => (
-  <div>
-    <Fitness />
+
+  <div className={styles.root}>
+    <div className={styles.container}>
+      <div className={styles.middleArticlesLeft}>
+        <Fitness />
+      </div>
+      <div className={styles.middleArticlesRight}>
+        <MiddleRightColumn />
+      </div>
+    </div>
   </div>
+
 );
 
 export default MiddleArticles;

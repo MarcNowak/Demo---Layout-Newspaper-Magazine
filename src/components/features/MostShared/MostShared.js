a9cd29c334cd182837cb434306f643f3f93d7940
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-02-08 17:04:08 +0100

    Add TEST - Duplicated 'MostPopular' component renamed to MostShared

---
+++ src/components/features/MostShared/MostShared.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import styles from './MostShared.module.scss';
+
+const MostShared = () => (
+  <div className={styles.root}>
+    <div>
+      <p>MostShared</p>
+    </div>
+  </div>
+);
+
+// MostShared.propTypes = {
+//   children: PropTypes.node,
+// };
+
+export default MostShared;

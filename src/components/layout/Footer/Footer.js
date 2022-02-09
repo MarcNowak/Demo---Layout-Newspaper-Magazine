4a15c23a292ffe19807437e2f277796e35600757
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-02-09 09:00:39 +0100

    Add Task_004_Footer - approach modify

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,41 +1,32 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+// import PropTypes from 'prop-types';
 
 import styles from './Footer.module.scss';
 
 const Footer = () => (
   <footer className={styles.root}>
-    <Grid>
-      <Row beetween='md' middle='xs'>
-        <Col md={6} lg={6}>
-          <div className={styles.leftSide}>
-            <span>© Copyright - Newspaper Wordpress Theme by TagDiv</span>
-          </div>
-        </Col>
-        <Col md={6} lg={6}>
-          <div className={styles.rightSide}>
-            <ul>
-              <li>
-                <a href='#'>Blog</a>
-              </li>
-              <li>
-                <a href='#'>About</a>
-              </li>
-              <li>
-                <a href='#'>Contact</a>
-              </li>
-            </ul>
-          </div>
-        </Col>
-      </Row>
-    </Grid>
+    <div className={styles.container}>
+      <div className={styles.leftSide}>
+        <span>© Copyright - Newspaper Wordpress Theme by TagDiv</span>
+      </div>
+      <div className={styles.rightSide}>
+        <ul>
+          <li>
+            <a href='#'>Blog</a>
+          </li>
+          <li>
+            <a href='#'>About</a>
+          </li>
+          <li>
+            <a href='#'>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </footer>
 );
 
-Footer.propTypes = {
-  children: PropTypes.node,
-};
+// Footer.propTypes = {};
 
 export default Footer;

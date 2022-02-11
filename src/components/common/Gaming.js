bb817ea5187b68888c15be3335f3376e5ae61763
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-02-11 09:50:49 +0100

    Task_024_Expander_Gaming - DONE

--- src/components/common/Gaming.js
+++ src/components/common/Gaming.js
@@ -15,13 +15,24 @@ const Gaming = () => (
   <div className={styles.root}>
     <div className={styles.container}>
       <div className={styles.titleWrapper}>
-        <div className={styles.sectionTitle}>gaming</div>
-        <div className={styles.expander}>
+        <div className={styles.sectionTitle}>
+          gaming
+        </div>
+        <div className={styles.expander}> {/* EXPANDER START */}
           <div className={styles.expanderText}>All</div>
           <div className={styles.expanderArrow}>
             <FontAwesomeIcon icon={faChevronDown}></FontAwesomeIcon>
           </div>
-        </div>
+          <div className={styles.expandContent}>
+            <ul>
+              <li><a href='#'>All</a></li>
+              <li><a href='#'>Global</a></li>
+              <li><a href='#'>Showbiz</a></li>
+              <li><a href='#'>Gadgets</a></li>
+              <li><a href='#'>Fitness</a></li>
+            </ul>
+          </div>
+        </div>    {/* EXPANDER END */}
       </div>
       <div className={styles.articleWrapper}>
         <div className={styles.articlePhoto}>

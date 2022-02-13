0214ceaa5fa56f6b4c8fe38a435536aff4bac364
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-02-13 14:13:14 +0100

    Task_006_v2_MostPopular

--- src/components/features/MostPopular/MostPopular.js
+++ src/components/features/MostPopular/MostPopular.js
@@ -7,43 +7,51 @@ import styles from './MostPopular.module.scss';
 const MostPopular = () => (
   <div className={styles.root}>
     <div className={styles.container}>
-      <div className={styles.leftSidePicture}>
+      <div className={styles.leftSidePictureContainer}>
+        <div className={styles.leftPhoto}>
+          <img src='images/articles/MostPopular_bigPicture.jpg' alt='01' />
+        </div>
         <div className={styles.articleDescriptionWrapper}>
           <div className={styles.postCategory}>style</div>
-          <div className={styles.leftSideTitle}>
-            What Makeup to Wear Daily, No Matter What You’ve Got Planned
-          </div>
+          <div className={styles.leftSideTitle}><h3>What Makeup to Wear Daily, No Matter What You’ve Got Planned</h3></div>
           <div className={styles.editorDate}>David Lee - June 3, 2020</div>
         </div>
       </div>
-
-      <div className={styles.rightSidePicture}>
-        <div className={styles.upperRow}>
-          <div className={styles.smallPicture01}>
-            <div className={styles.smallPicture01DescWrapper}>
-              <div className={styles.smallPicture01Cat}>travel</div>
-              <div className={styles.smallPicture01Desc}>What Makeup to Wear Daily, No Matter What You’ve Got Planned</div>
-            </div>
+      <div className={styles.rightSidePictureContainer}>
+        <div className={styles.articleWrapper01}>
+          <div className={styles.rightPhoto}>
+            <img src='images/articles/MostPopular_smallPhoto01.jpg' alt='01' />
           </div>
-          <div className={styles.smallPicture02}>
-            <div className={styles.smallPicture02DescWrapper}>
-              <div className={styles.smallPicture02Cat}>style</div>
-              <div className={styles.smallPicture02Desc}>Work Out as Much as Possible During Your All Inclusive Vacations</div>
-            </div>
+          <div className={styles.artDetails}>
+            <div className={styles.postCategory}>travel</div>
+            <div className={styles.rightSideTitle}>Work Out as Much as Possible During Your All Inclusive Vacations</div>
           </div>
         </div>
-        <div className={styles.bottomRow}>
-          <div className={styles.smallPicture03}>
-            <div className={styles.smallPicture03DescWrapper}>
-              <div className={styles.smallPicture03Cat}>show biz</div>
-              <div className={styles.smallPicture03Desc}>Expert Advice: The Best Cheap Retro Chic Fashion for this Fall</div>
-            </div>
+        <div className={styles.articleWrapper02}>
+          <div className={styles.rightPhoto}>
+            <img src='images/articles/MostPopular_smallPhoto02.jpg' alt='01' />
           </div>
-          <div className={styles.smallPicture04}>
-            <div className={styles.smallPicture04DescWrapper}>
-              <div className={styles.smallPicture04Cat}>facts</div>
-              <div className={styles.smallPicture04Desc}>Bloggers Hijacked the Industry, Cutting a Piece of Earnings</div>
-            </div>
+          <div className={styles.artDetails}>
+            <div className={styles.postCategory}>style</div>
+            <div className={styles.rightSideTitle}>Expert Advice: The Best Cheap Retro Chic Fashion for this Fall</div>
+          </div>
+        </div>
+        <div className={styles.articleWrapper03}>
+          <div className={styles.rightPhoto}>
+            <img src='images/articles/MostPopular_smallPhoto03.jpg' alt='01' />
+          </div>
+          <div className={styles.artDetails}>
+            <div className={styles.postCategory}>showbiz</div>
+            <div className={styles.rightSideTitle}>Bloggers Hijacked the Industry, Cutting a Piece of Earnings</div>
+          </div>
+        </div>
+        <div className={styles.articleWrapper04}>
+          <div className={styles.rightPhoto}>
+            <img src='images/articles/MostPopular_smallPhoto04.jpg' alt='01' />
+          </div>
+          <div className={styles.artDetails}>
+            <div className={styles.postCategory}>facts</div>
+            <div className={styles.rightSideTitle}>The Weirdest Places Ashes Have Been Scattered in South America</div>
           </div>
         </div>
       </div>
